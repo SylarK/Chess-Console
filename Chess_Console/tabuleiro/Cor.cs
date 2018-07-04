9ad595f0d191e522ede62d2249190de1aab1fadb
Lucas Amado <lucasamadort@gmail.com>
2018-07-04 17:31:44 -0300

    Criacao de algumas classes. Comecando a implementar o tabuleiro

---
+++ Chess_Console/tabuleiro/Cor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Console.tabuleiro
+{
+    enum Cor //tipo enumerado cor que carregar a cor de um objeto
+    {
+        Branca,
+        Preta,
+        Vermelha,
+        Azul,
+        Amarela,
+        Verde,
+        Laranja
+    }
+}
---
+++ Chess_Console/tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Console.tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; } // uma peça possui uma posição
+        public Cor cor { get; protected set; } // uma peça possui uma cor
+        public int qteMovimentos { get; protected set; } //controlar a quantidade de movimentos (ex. Peão)
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.cor = cor;
+            qteMovimentos = 0;
+        }
+
+    }
+}
